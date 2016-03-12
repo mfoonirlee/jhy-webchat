f2da98908086e5e4789458ab64120fe427f015dd
hong.li <hong.li@Ctrip.com>
2016-03-12 18:44:49 +0800

    delete server files

--- service/data/enums/freesharetype.js
+++
@@ -1,5 +0,0 @@
-<!--
-em_freesharetypes=new Array();
-em_freesharetypes[500]='新品介绍';
-em_freesharetypes[1000]='试吃活动';
--->
--- service/data/enums/freesharetype.php
+++
@@ -1,6 +0,0 @@
-<?php
-global $em_freesharetypes;
-$em_freesharetypes = array();
-$em_freesharetypes['500'] = '新品介绍';
-$em_freesharetypes['1000'] = '试吃活动';
-?>
